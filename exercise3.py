ba7666873ccda58e5a2b62853538455d5c6aaf20
ZIkt <thanimosesisaac32gmail.com>
2020-03-13 11:29:32 +0000

    First Commit

---
+++ exercise3.py
@@ -0,0 +1,7 @@
+"""Run few replications of the experiment by using different seeds (integer numbers). You
+can use the pre-prepared acrobot.ini file included in the ./xacrobot directory. While the program is
+running check the source code of the environment available from the https://gym.openai.com/envs/
+website to figure out the content of the observation vectors, the content of the action vector, and the
+way in which the reward is calculated. Plot performance across generations and then observe the
+behavior of evolved robots."""
+
