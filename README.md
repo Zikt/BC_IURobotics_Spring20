cfe4b8dfb596b4feb439c2f8e885ace5675d7a35
ZIkt <thanimosesisaac32gmail.com>
2020-03-13 13:30:54 +0000

    Some changes to exercises

--- README.md
+++ README.md
@@ -2,9 +2,11 @@ BC_IURobotics_Spring20
 
 Assignments and Labs for Behavioral & Cognitive Robotics Course in Innopolis University for Spring Semester 2020
 
-The guide, instructions for this course and other materials can be found in the repository [here](https://github.com/snolfi/evorobotpy). Course Instructor - [Stefano Nolfi](https://scholar.google.com/citations?user=YVqJ_u8AAAAJ&hl=en). 
+The guide, instructions for this course and other materials can be found in the repository [here](https://github.com/snolfi/evorobotpy). Course Instructor - [Prof.Stefano Nolfi](https://scholar.google.com/citations?user=YVqJ_u8AAAAJ&hl=en). 
 
 All the software required is available ready to be used through the docker container docker container prepared by Vladislav Kurenkov that can be pulled, built and run. Check the repository above for further instructions. 
 
-First Task
-Getting use to working with Gym.
+First Week Tasks
+Getting used to working with Gym.
+Exercise1: Testing and learning how to use gym environments
+Exercise2: Implement Evolutionary Hill-Climber with simple Neural Network and train it.
--- exercise1.py
+++ exercise1.py
@@ -1,27 +1,28 @@
 import gym
 env = gym.make('CartPole-v0')
 
-# init_obs = env.reset()
+init_obs = env.reset()
 
-# def get_action(obs):
-#     """Return observation"""
-#     return env.action_space.sample()
+def get_action(obs):
+    """Return observation"""
+    return env.action_space.sample()
 
-# def run_episode(render=False):
-#     done = False
-#     obs = env.reset
-#     while not done:
-#         if(render):
-#             env.render()
-#         action = get_actions(obs)
-#         obs, reward, done, _ = env.step(ation)
+def run_episode(render=False):
+    done = False
+    obs = env.reset
+    while not done:
+        if(render):
+            env.render()
+        action = get_actions(obs)
+        obs, reward, done, _ = env.step(ation)
 
-# run_episode(render=True)
+run_episode(render=True)
 
-# env.close()
-
-
-for _ in range(200):
-    env.render()
-    observation, reward, done, info = env.step(env.action_space.sample())
 env.close()
+
+########################################################
+
+# for _ in range(200):
+#     env.render()
+#     observation, reward, done, info = env.step(env.action_space.sample())
+# env.close()
